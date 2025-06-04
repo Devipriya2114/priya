a28f8ba639281a0e85e59f72cd9a58960606891e
Devipriya2114 <ddpriya2114@gmail.com>
2025-06-04 14:49:17 +0530

    abc2 added

---
+++ abc2.java
@@ -0,0 +1,14 @@
+package javabasic;
+
+public class abc2 extends abstra {
+
+	public void day() {
+		System.out.println("3days");
+		
+	}
+public static void main(String[] args) {
+	abc2 A = new abc2();
+	A.otp();	
+	A.day();
+}
+}
